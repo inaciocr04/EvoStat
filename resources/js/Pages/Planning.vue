68d007060309139358af693a5a780ebb4186cb70
inaciocr04 <inacio.rodrigues@etu.unistra.fr>
2025-09-18 18:15:19 +0200

    design page

--- resources/js/Pages/Planning.vue
+++ resources/js/Pages/Planning.vue
@@ -280,9 +280,9 @@ const scheduledWorkoutsOnly = computed(() => {
 <template>
     <Head title="Planning" />
 
-    <div class="min-h-screen bg-gray-50">
+    <div class="min-h-screen">
         <!-- Header avec statistiques -->
-        <div class="bg-white shadow-sm border-b">
+        <div>
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                 <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-4 sm:space-y-0">
                     <div>
